b0089a5085d29c14bf77e33f21aca4165ed65865
Patrick Wagner <patrickwagner2607@gmail.com>
2026-09-12 10:47:01 +0200

    add: find something in stacked objects

--- script.js
+++ script.js
@@ -89,7 +89,70 @@
             // Ein Array.findIndex ist wie Array.filter aufgebaut, aber es gibt den Index aus
             console.log(  myObjectArr.findIndex  ((element) => element['name']=='Justus'));
 
+    // Verschachtelte JSON/Object
+        // Hier ein Beispiel:
+        let library = {
+        "info": {
+            "name": "Stadtbibliothek",
+            "location": {
+            "city": "Musterstadt",
+            "coordinates": { "lat": 48.1351, "lon": 11.582 }
+            }
+        },
+        "sections": {
+            "fiction": [
+            {
+                "shelf": 1,
+                "book": {
+                "title": "Die Verwandlung",
+                "author": {
+                    "name": "Franz Kafka",
+                    "born": "1883-07-03",
+                    "died": "1924-06-03"
+                },
+                "year": 1915,
+                "status": "ausgeliehen"
+                }
+            },
+            {
+                "shelf": 2,
+                "book": {
+                "title": "1984",
+                "author": {
+                    "name": "George Orwell",
+                    "born": "1903-06-25",
+                    "died": "1950-01-21"
+                },
+                "year": 1949,
+                "status": "verfügbar"
+                }
+            }
+            ],
+            "nonFiction": [
+            {
+                "shelf": 3,
+                "book": {
+                "title": "Eine kurze Geschichte der Zeit",
+                "author": {
+                    "name": "Stephen Hawking",
+                    "born": "1942-01-08",
+                    "died": "2018-03-14"
+                },
+                "year": 1988,
+                "status": "verfügbar"
+                }
+            }
+            ]
+        }
+        }
 
+        // Diese können ganz einfach ausgelesen werden z.B. erstmal in der Console
+        // Auslesen in der Console: gib dafür einfach den namen des Objects an und bestätige
+        // Nun kannst du das Object nach und nach aufklappen in der Console
+        // Alternativ auch dann Stück im console.log bis man die gewünschten Daten hat
+            console.log(library.info.location.city);    // -> Musterstadt
+            console.log(library.sections.fiction[1].book.status);   // -> verfügbar
+            
 // Console.table
     // Zur schöneren Ausgabe kann man simple Objekte und Arrays auch mit console.table ausgeben
         console.table(myObject.job);
